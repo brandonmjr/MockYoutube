dfed045233206c2dd107a309764f22092e282bb4
mrmaj <brandonmjr@yahoo.com>
2018-01-16 13:17:14 -0500

    make it better

---
+++ src/components/search_bar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+const Component = React.Component;
+//import React, {Component} from 'react'; <-- Can do the previous two lines in one step
+
+
+//bellow is a functional component
+/*const SearchBar = () => {
+	return <input />;
+};
+*/
+
+
+
+// below is a class based component
+class SearchBar extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = { term: ''};
+	}
+
+	render() {
+		return (
+			<div className="search-bar col-md-12">
+				<br/>
+				<input 
+				value = {this.state.term}
+				onChange={e => this.onInputChange(e.target.value)}/>
+				<br/>
+				<br/>
+				
+			</div>
+		);
+	}
+
+	onInputChange(term){
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
+	
+}
+
+
+
+export default SearchBar;
