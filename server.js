472ebcf531643b12c4bc3cae838462d3fa793965
mrmaj <brandonmjr@yahoo.com>
2018-01-16 13:54:11 -0500

    make better

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/assets', express.static(__dirname + '/src/assets/'));
-
-
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
